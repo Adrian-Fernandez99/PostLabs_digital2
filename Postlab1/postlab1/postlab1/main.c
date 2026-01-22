4222c12952adb9e16fb06f497aab40f486775e6e
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2026-01-21 23:03:43 -0600

    Creación
    Se crea el archivo con información del lab

---
+++ Postlab1/postlab1/postlab1/main.c
@@ -0,0 +1,26 @@
+/*
+Universidad del Valle de Guatemala
+IE2023 : Programación de Microcontroladores
+
+postlab1.c
+
+Created: 1/21/2026 10:59:47 PM
+Author : Adrián Fernández
+
+Descripción: Simulador de carreras. Se presionan botones
+para avanzar a los concursantes y cuando llegan a la meta
+indican quien ganó.
+ */ 
+
+// Librerias
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
