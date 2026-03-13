6511aa8baa6227f8569f0a0cf471c5a5ded5f575
Adrian-Fernandez99 <fer23381@uvg.edu.gt>
2026-03-12 20:34:49 -0600

    sss

---
+++ Postlab6/Postlab6/Postlab6/main.c
@@ -0,0 +1,18 @@
+/*
+ * Postlab6.c
+ *
+ * Created: 3/12/2026 8:31:25 PM
+ * Author : adria
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
